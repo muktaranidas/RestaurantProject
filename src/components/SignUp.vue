612ee0283c42150182de4d0363d220b890c0cfef
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-05-16 00:16:15 -0700

    signup done

---
+++ src/components/SignUp.vue
@@ -0,0 +1,60 @@
+<template>
+  <img class="signup-logo" src="../assets/signup.jpg" alt="" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" v-model="name" placeholder="Enter Name" />
+    <input type="email" v-model="email" placeholder="Enter Email" />
+    <input type="password" v-model="password" placeholder="Enter Password" />
+    <button v-on:click="signup">Sign Up</button>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async signup() {
+      let result = await axios.post("http://localhost:3000/users", {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+      });
+      console.warn(result);
+      if (result.status == 201) {
+        alert("SignUp Done");
+        localStorage.setItem("User-Info", JSON.stringify(result.data));
+      }
+    },
+  },
+};
+</script>
+<style>
+.signup-logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: white;
+  cursor: pointer;
+}
+</style>
